16d52b7551b69fcee63d88635a4610bb1d4e7691
Bruno Migliorini <cyberbex@gmail.com>
2021-07-10 19:21:59 -0300

    meu primeito commit

---
+++ app/routes/noticias.js
@@ -0,0 +1,19 @@
+module.exports = function(app){
+    
+    
+    app.get('/noticias', function(req,res){
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host : '18.231.188.23:8080',
+            user : 'root',
+            password : 'cyberbex88',
+            database : 'portal_noticias'
+        });
+    
+        connection.query('select * from noticias', function(error, result){
+            res.send(result);
+        });
+        //res.render('noticias/noticias');
+    });
+}
